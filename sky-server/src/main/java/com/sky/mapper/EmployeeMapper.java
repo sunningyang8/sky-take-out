7069977c86e282fc68b5d9eeb7a2da914d4b0a02
sunningyang8 <896047224@qq.com>
2026-01-22 18:16:31 +0800

    查询完成

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -24,4 +26,10 @@ public interface EmployeeMapper {
              "values(#{username}, #{name}, #{phone}, #{sex}, #{idNumber}, #{password}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser}, #{status})")
      void insert(Employee employee);
 
+    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+    /*
+     * 员工列表分页
+     * */
+    
 }
